a49a5b521e9d0adf40e0af920ee4b5c943436252
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 14:30:35 +0000

    Debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  validates_presence_of :name
+         
   def first_name
     self.name.split.first
   end
   
   def last_name
     self.name.split.last
+  end
+  
+  def get_facebook_messages
+    begin
+      contacts_fb
+      @messages = retrieves_messages
+    rescue => e
+      flash[:error] = "Error occured contacting Facebook: #{e}"
+    end
   end
 end

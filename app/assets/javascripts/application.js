9d9bb7299ed46e96b903bb0aec694a79d1ea35e4
Ojas Kulkarni <ojas.ajay.kulkarni@gmail.com>
2017-08-08 17:48:22 +0000

    Committed custom changes, added dropdown on applicaion, and kind of fixed the gritter error.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,11 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
+//= require gritter
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
 //= require cocoon
-//= require gritter
 //= require cable
 //= require turbolinks
 //= require_tree .
